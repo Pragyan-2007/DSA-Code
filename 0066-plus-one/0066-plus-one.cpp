72b245f0c89f736df47799d95bd5977b3e416a30
Pragyan-2007 <exex70346@gmail.com>
2026-09-25 00:19:05 +0530

    Time: 0 ms (100%), Space: 11.1 MB (98.55%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& arr) {
+        int carry = 1;
+        vector<int> ans;
+        for(int i =arr.size()-1;i>=0;i--){
+            int sum = arr[i]+carry;
+            if(sum<10){
+                carry = 0;
+                ans.push_back(sum);
+            }
+            else{
+                carry = 1;
+                ans.push_back(0);
+            }
+        }
+        if(carry ==1 ) ans.push_back(1);
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
